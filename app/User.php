557fe072cc602f277eeacc0c94aacee112e07db3
komsak007 <lert_zodiac@hotmail.com>
2021-03-06 00:15:43 +0700

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -34,6 +34,12 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
+    public function getUrlAttribute()
+    {
+        // return route("questions.show", $this->id);
+        return '#';
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
