0bcc1da182bc81bbc226ca61f5b06903a7e6a9bd
komsak007 <lert_zodiac@hotmail.com>
2021-03-17 14:41:09 +0700

    Make the vote answer working

--- database/seeds/VotablesTableSeeder.php
+++ database/seeds/VotablesTableSeeder.php
@@ -14,17 +14,27 @@ class VotablesTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('votables')->where('votable_type', 'App\Question')->delete();
+        \DB::table('votables')->delete();
 
         $users = User::all();
         $numberOfUsers = $users->count();
         $votes = [-1, 1];
+        
         foreach (Question::all() as $question)
         {
             for ($i = 0; $i < rand(1, $numberOfUsers); $i++)
             {
                 $user = $users[$i];
                 $user->voteQuestion($question, $votes[rand(0, 1)]);
+            }
+        }
+
+        foreach (Answer::all() as $answer)
+        {
+            for ($i = 0; $i < rand(1, $numberOfUsers); $i++)
+            {
+                $user = $users[$i];
+                $user->voteAnswer($answer, $votes[rand(0, 1)]);
             }
         }
         
