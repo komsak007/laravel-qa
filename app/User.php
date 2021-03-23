a5e31fcac914c20b7345d11414f16f0c8380b544
komsak007 <lert_zodiac@hotmail.com>
2021-03-23 12:13:33 +0700

    Rewrite the vote controller into Vue.js compponent

--- app/User.php
+++ app/User.php
@@ -75,14 +75,14 @@ class User extends Authenticatable
     {
         $voteQuestions = $this->voteQuestions();
         
-        $this->_vote($voteQuestions, $question, $vote);
+        return $this->_vote($voteQuestions, $question, $vote);
     }
 
     public function voteAnswer(Answer $answer, $vote)
     {
         $voteAnswers = $this->voteAnswers();
         
-        $this->_vote($voteAnswers, $answer, $vote);
+        return $this->_vote($voteAnswers, $answer, $vote);
     }
 
     private function _vote($relationship, $model, $vote)
@@ -100,6 +100,8 @@ class User extends Authenticatable
 
         $model->votes_count = $upVotes + $downVotes;
         $model->save();
+
+        return $model->votes_count;
     }
     
 
