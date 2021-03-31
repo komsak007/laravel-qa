7916d5b11151b5185059afa32ad549636c8cbdfb
komsak007 <lert_zodiac@hotmail.com>
2021-03-31 13:39:11 +0700

    Consume all questions endpoint on QuestionsPage component

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -81,11 +81,14 @@
 
     <!-- Scripts -->
     <script>
-        window.Auth = {!! json_encode([
+        window.Auth = @json([
             'signedIn' => Auth::check(),
-            'user' => Auth::user(),
-            'url' => route('login')
-        ]) !!}
+            'user' => Auth::user()
+        ]);
+        window.Urls = @json([
+            'api' => url('/api'),
+            'login' => route('login')
+        ]);
     </script>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
